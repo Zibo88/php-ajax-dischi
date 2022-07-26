f50a72fba0bd699cb64c10595e24d0c10911921a
Zibo88 <vincenzo.tardino1988@gmail.com>
2022-07-26 16:49:35 +0200

    esercizio finito

--- js/script.js
+++ js/script.js
@@ -1,27 +1,27 @@
 // creo struttura vue
-
 var app = new Vue (
     {
         el: '#root',
         data:{
+            // array vuoto
             albums: []
         },
         // creo una funzione per la chiamata axios
         methods:{
-            axioCall(){
+            axiosCall(){
+                // l'url è quello della pagina dove si trova la struttura dati in formato json (api.php)
                axios.get('http://localhost:8888/php-ajax-dischi/api.php').then((response) => {
                 //    osservo il tipo di dato
-                    console.log(response)
+                    // console.log(response)
                     // ogni singolo data viene inserito ll'interno dell'array vuoto
                     this.albums = response.data
-                    console.log(this.albums)
-
+                    // console.log(this.albums)
                } )
             }
         },
         // richiamo la funzione
         mounted(){
-            this.axioCall()
+            this.axiosCall()
         }
     }
 )
