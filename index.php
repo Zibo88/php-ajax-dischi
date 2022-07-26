28c42c74e30ae49f09d5bf462c1c0a4c38064c9e
Zibo88 <vincenzo.tardino1988@gmail.com>
2022-07-26 13:12:25 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,36 @@
+<?php include __DIR__ . '/database.php' ?>
+<?php var_dump(__DIR__ . '/database.php') ?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+
+    <header>
+        <div class="header-container">
+            <img src="" alt="">
+        </div>
+    </header>
+
+    <main>
+        <div class="main-container">
+            <div class="discs-container">
+                <div class="disc">
+                    <h2>titolo</h2>
+                    <div>Autore</div>
+                    <div>data</div>
+                </div>
+            </div>
+        </div>
+    </main>
+
+</body>
+</html>
